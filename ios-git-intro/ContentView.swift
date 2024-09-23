35c1a87189acf4c487a65642cce698abf001668f
Thomas Dac Nguyen <thomasnguyen1350@gmail.com>
2024-09-23 17:29:53 -0400

    Initial Commit

---
+++ ios-git-intro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios-git-intro
+//
+//  Created by Thomas Dac Nguyen on 9/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Thomas!")
+        }
+        .padding()
+    }
+}
+
+// This is a comment I am making
+
+#Preview {
+    ContentView()
+}
---
+++ ios-git-intro/ios_git_introApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_git_introApp.swift
+//  ios-git-intro
+//
+//  Created by Thomas Dac Nguyen on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_git_introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
